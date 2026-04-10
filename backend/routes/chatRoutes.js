f184b6fffabb9b795310856fe0b40e1d34e57869
Rafat Abdullah Pantho <rafatpantho@gmail.com>
2026-04-10 22:48:16 +0600

    removed

--- backend/routes/chatRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getChatHistory } = require('../controllers/chatController');
-const { protect } = require('../middleware/auth');
-
-const router = express.Router();
-
-router.get('/:room', protect, getChatHistory);
-
-module.exports = router;
--- frontend/src/context/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * Context Providers Index
- */
-
-export { AuthProvider, useAuth } from './AuthContext';
-export { GameProvider, useGame } from './GameContext';
-export { SocketProvider, useSocket } from './SocketContext';
